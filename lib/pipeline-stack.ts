e40411cc6928830529330103c375ab377436ea65
ppgm-developer <ppgm+devcc@amazon.com>
2024-04-21 00:26:51 +0000

    changes build cmd to npm install

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -16,7 +16,7 @@ export class PipelineStack extends cdk.Stack {
             pipelineName: 'POC-Pipeline',
             synth: new ShellStep('Synth', {
                 input: CodePipelineSource.codeCommit(repo, 'main'),
-                commands: ['npm ci', 'npm run build', 'cdk synth']
+                commands: ['npm install', 'npm run build', 'cdk synth']
             })
         })
 
