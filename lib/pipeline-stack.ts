d430344f0754ab99102de9debbc7b3d9c164425f
ppgm-developer <ppgm+devcc@amazon.com>
2024-04-21 00:17:35 +0000

    changed build command to npx aws-cdk synth

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -16,7 +16,7 @@ export class PipelineStack extends cdk.Stack {
             pipelineName: 'POC-Pipeline',
             synth: new ShellStep('Synth', {
                 input: CodePipelineSource.codeCommit(repo, 'main'),
-                commands: ['npm install', 'npm uninstall -g aws-cdk','npm install -g aws-cdk','npm run build', 'npx cdk synth']
+                commands: ['npm ci', 'npm run build', 'npx aws-cdk synth']
             })
         })
 
